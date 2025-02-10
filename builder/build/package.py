d0eee74c344f78b9afe293ca462c1e469703a545
BigfootACA <bigfoot@classfun.cn>
2025-02-10 17:35:00 +0800

    builder: build: package.py: split for more formats
    Signed-off-by: BigfootACA <bigfoot@classfun.cn>

--- builder/build/package.py
+++ builder/build/package.py
@@ -5,6 +5,18 @@ from builder.lib.config import ArchBuilderConfigError
 log = logging.getLogger(__name__)
 
 
+def package_7zip(ctx: ArchBuilderContext, out: str):
+	args = ["7z", "a", "-ms=on", "-mx=9", out, "."]
+	if os.path.exists(out) and os.path.isfile(out):
+		log.debug(f"removing {out}")
+		os.unlink(out)
+	return ctx.run_external(args, cwd=ctx.get_output())
+
+
+package_formats = [
+	(".7z",    package_7zip),
+]
+
 def done_package(ctx: ArchBuilderContext):
 	file: str = ctx.get("package.file", "")
 	out = file
@@ -12,10 +24,14 @@ def done_package(ctx: ArchBuilderContext):
 		if not os.path.exists(ctx.artifacts):
 			os.makedirs(ctx.artifacts, mode=0o755, exist_ok=True)
 		out = os.path.join(ctx.artifacts, file)
-	if not out.endswith(".7z"):
-		raise ArchBuilderConfigError("current only supports 7z")
+	packager: function[ctx: ArchBuilderContext, out: str] = None
+	for p in package_formats:
+		if out.lower().endswith(p[0]):
+			packager = p[1]
+			break
+	if not packager:
+		raise ArchBuilderConfigError("unsupported package format")
 	log.info(f"creating package {file}")
-	args = ["7z", "a", "-ms=on", "-mx=9", out, "."]
-	ret = ctx.run_external(args, cwd=ctx.get_output())
+	ret = packager(ctx, out)
 	if ret != 0: raise OSError("create package failed")
 	log.info(f"created package at {out}")
